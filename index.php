87508203b8d3d9cb37e506e5f727058c9b9871be
Vitoco <Vitoco@Vito-Pc>
2016-05-11 19:39:58 -0300

    index

--- index.php
+++ index.php
@@ -1,84 +1,41 @@
-<?php 
-require_once (dirname ( __FILE__ ) . '/../../config.php');?>
-<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link rel="stylesheet" href="estilo.css">
-<title>
-Tutoriales WebCursos
-</title>
-<body>
-<div id="linea">
-</div>
-<div id="header">
-	<div id="logos">
-			<a class="logo" href="<?php echo $CFG->wwwroot . '/local/tutoriales/index.php'; ?> ">
-				<img src="images/GIO.png">
-			</a>
-		<div class="logo2">
-		</div>
-	</div>
-</div>
-
-<div id="linea">
-</div>
-
-<div id="banner">
-</div>
-
-<div id="linea2">
-</div>
-
-<div id="main">
-	<div id="container1" class="clearfix">
-		<a class="dest cat1" href="<?php echo $CFG->wwwroot . '/local/tutoriales/profesores.php'; ?>">
- 			<img width="256" height="256" src="images/profesoresg.png">
-		</a>
-		<a class="dest cat2" href="<?php echo $CFG->wwwroot . '/local/tutoriales/alumnos.php'; ?>">
-			<img width="256" height="256" src="images/alumnosg.png">
-		</a>
-		<a class="dest cat3" href="<?php echo $CFG->wwwroot . '/local/tutoriales/coordinadores.php'; ?>">
-			<img width="256" height="256" src="images/coordinadoresg.png">
-		</a>
-		<a class="dest cat4" href="<?php echo $CFG->wwwroot . '/local/tutoriales/administrativos.php'; ?>">
-			<img width="256" height="256" src="images/administrativosg.png">
-		</a>
-	</div>
-	<div id="separador">
-	</div>
-	<div id="container3">
-		<a href="<?php echo $CFG->wwwroot . '/local/tutoriales/faq.php'; ?>">
-		<img src="images/faq.png">
-		</a>
-	</div>
-	<div id="separador">
-	</div>
-	<div id="container2">
-		<table width="1024px" height="100%">
-		<tr>
-			<td class="imagen" colspan="2"></td>
-		</tr>
-		<tr>		
-			<td width="50%" height="80%">
-				<iframe width="680" height="280" src="http://www.youtube.com/embed/ernR7ahcqyw?feature=player_detailpage" frameborder="0"></iframe>
-			</td>
-		</tr>
-		</table> 
-	</div>
-</div>
-<div id="linea2">
-</div>
-
-<div id="foot">
-	<div class="footer">&copy; <?php echo date('Y');?> Universidad Adolfo Ib&aacute;&ntilde;ez</div>
-	<div id="social">
-		<div class="facebook">
-			<a href="https://www.facebook.com/coordinador.webcursosuai">
-			<img width="50" height="50" src="images/facebook.png">
-			</a></div>
-		<div class="twitter">
-			<a href="https://twitter.com/UAI_CL">
-		<img width="50" height="50" src="images/twitter.png">
-		</a></div>
-	</div>
-</div>
-</body>
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *
+ * @package local
+ * @subpackage tics331
+ * @copyright 2012-onwards Jorge Villalon <jorge.villalon@uai.cl>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+// Minimum for Moodle to work, the basic libraries
+require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+// Parameter passed from the url.
+$name = required_param('name', PARAM_TEXT);
+// Moodle pages require a context, that can be system, course or module (activity or resource)
+$context = context_system::instance();
+$PAGE->set_context($context);
+// Check that user is logued in the course.
+require_login();
+// Page navigation and URL settings.
+$PAGE->set_url(new moodle_url('/local/tics331', array('filter'=>$name)));
+$PAGE->set_pagelayout('incourse');
+$PAGE->set_title('Hello world');
+// Show the page header
+echo $OUTPUT->header();
+// Here goes the content
+echo 'Hello world';
+// Show the page footer
+echo $OUTPUT->footer();
