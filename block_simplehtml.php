16e8f9682749c634b96c855cbec9f370db726e0e
vitorioni <vortuzar@alumnos.uai.cl>
2016-05-23 00:37:58 -0300

    Update block_simplehtml.php

--- block_simplehtml.php
+++ block_simplehtml.php
@@ -3,24 +3,28 @@
 
 
 //Aca se crea el bloque
-class block_simplehtml extends block_base {
+class block_simplehtml extends block_list {
 	public function init() {
-		$this->title = get_string('simplehtml', 'block_simplehtml');
+		$this->title = get_string('Proyectos', 'block_simplehtml');
 	}
 	//con esto nuestro bloque desplegara algo
 	public function get_content() {
 		if ($this->content !== null) {
-			return $this->content;
-		}
-	
-		$this->content         =  new stdClass;
-		$this->content->text   = 'The content of our SimpleHTML block!';
-		$this->content->footer = 'Footer here...';
-	
-		return $this->content;
-		if (! empty($this->config->text)) {
-			$this->content->text = $this->config->text;
-		}
+    return $this->content;
+  }
+ //agrega un link a una página de moodle dentro del bloque
+  $this->content         = new stdClass;
+  $this->content->items  = array();
+  $this->content->icons  = array();
+  $this->content->footer = '';
+ //se añaden los links de pestañas en el bloque de la vista alumnos
+  $this->content->items[] = html_writer::tag('a', 'Ver Proyectos', array('href' => 'http://localhost/moodle/local/alumnosVerProyectos/index.php'));
+  $this->content->icons[] = html_writer::empty_tag('img', array('src' => '', 'class' => 'icon'));
+  $this->content->items[] = html_writer::tag('a', 'Mis proyectos', array('href' => 'http://localhost/moodle/local/alumnosMisProyectos/index.php'));
+  $this->content->icons[] = html_writer::empty_tag('img', array('src' => '', 'class' => 'icon'));
+  
+ 
+  return $this->content;
 	}
 	
 	public function specialization() {
