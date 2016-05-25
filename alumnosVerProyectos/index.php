fe631b36cfbf4d4a6c2f1e1a55046ad237bb090d
SChacana <s.chacana@hotmail.com>
2016-05-25 15:45:35 -0300

    indexalumnosverproyectos.php
    Archivo actualizado de Alumnos Ver Proyectos

--- alumnosVerProyectos/index.php
+++ alumnosVerProyectos/index.php
@@ -21,6 +21,10 @@
  * @copyright 2012-onwards Jorge Villalon <jorge.villalon@uai.cl>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+$mysqli = new mysqli("localhost", "root", "", "moodle");
+$mysqli = new mysqli("localhost", "root", "", "moodle", 3306);
+
 // Minimum for Moodle to work, the basic libraries
 require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
 
@@ -41,7 +45,44 @@ $PAGE->set_title('Ver proyectos');
 echo $OUTPUT->header();
 
 // Here goes the content
+
+echo "<form method='POST' action='index.php'>";
+
 echo 'Ver Proyectos';
+global $USER;
+$nom=$USER->username;
+echo "<br>";
+echo "<select name='select1'>";
+if ($resultado1 = $mysqli->query("SELECT * FROM proyectos WHERE integrante='$nom'")) {
+	printf("La seleccion devolvio %d filas.\n", $resultado->num_rows);
+}
+while($fila1=mysqli_fetch_assoc($resultado1)){
+	echo "<option value=".$fila1['codigo'].">".$fila1['nombreproyecto']."</option>";
+}
+echo "</select>";
+
+echo "<input type='submit' value='Ver Proyecto' class='btn btn-success' name='btn1'>";
+
+echo "</form>";
+echo "<form method='POST' action='http://localhost/moodle/local/alumnosMisProyectos/index.php'>";
+$codigo=$_POST['select1'];
+echo "<input type='hidden' name='cod' value='$codigo'>";
+if($descripcion1= $mysqli->query("SELECT * FROM proyectos WHERE integrante='$nom' AND codigo='$codigo'" ))
+{
+while ($row = mysqli_fetch_assoc($descripcion1))
+{
+	echo "<div align='center'><b>{$row['nombreproyecto']}</b></div><br>";
+	echo "<b>Descripcion del proyecto </b>: {$row['descripcion']} <br>";
+	echo "<b>Profesor </b>: {$row['profesor']} <br>";
+	echo "<b>Tu Rol </b>: {$row['roles']} <br>";
+	
+	echo "<div align='center'> <input type='submit' value='Detalles del Proyecto' class='btn btn-success' name='btn1'></div>";
+}
+}
+echo "</form>";
+
+
+
 
 // Show the page footer
 echo $OUTPUT->footer();
